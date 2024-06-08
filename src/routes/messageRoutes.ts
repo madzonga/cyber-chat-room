18da38b56c12ed7967bc09294a569cad57a12b42
glenn <glenn.madzonga@gmail.com>
2024-06-09 01:22:51 +0200

    replaced the delete most recent message endpoint with a more specific delete by id endpoint

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { sendMessage, getChatHistory, deleteMostRecentMessage } from '../controllers/messageController';
+import { sendMessage, getChatHistory, deleteMessageById } from '../controllers/messageController';
 import { authenticateToken } from '../middlewares/auth';
 import { validate, validateUser } from '../middlewares/validate';
 import { messageSchema } from '../validators/schemas';
@@ -12,7 +12,7 @@ router.post('/send', authenticateToken, validateUser, validate(messageSchema), s
 // Get chat history route
 router.get('/history', authenticateToken, getChatHistory);
 
-// Delete most recent message
-router.delete('/recent', authenticateToken, deleteMostRecentMessage);
+// Delete message by id
+router.delete('/delete/:id', authenticateToken, deleteMessageById); 
 
 export default router;
